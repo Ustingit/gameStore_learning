58bb65544dc419a9f460995fe88582e70caa0e4b
yuryi.ustinovich <ustinhtc@gmail.com>
2022-03-28 21:53:35 +0300

    add game controller and defaul list page

---
+++ GameStore/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameStore.StoreDomain.Abstract;
+
+namespace GameStore.Controllers
+{
+    public class GameController : Controller
+    {
+	    private IGameRepository repository;
+
+	    public GameController(IGameRepository repo)
+	    {
+		    repository = repo;
+	    }
+
+	    public ViewResult List()
+	    {
+		    return View(repository.Games);
+	    }
+    }
+}
